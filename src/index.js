c5b3757268e988452068dea61108fe5806443ade
MariiaSam <kontragent@ukr.net>
2023-09-25 21:02:41 +0300

    add

--- src/index.js
+++ src/index.js
@@ -9,26 +9,33 @@ axios.defaults.headers.common['x-api-key'] =
   'live_msdyLXd6fQ4XqkQOafThG3QlqI9pVAyJsQYWr21gOKoQrJvn3mlevFEdkllUWaMq';
 axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
 
-fetchBreeds().then(response => {
-elements.selectCat.hidden = false;
-elements.selectCat.innerHTML = response.map(({ id, name }) => {
-    return `<option class = "option" value="${id}">${name}</option>`;
-  }).join('');
-  new SlimSelect({
-    select: '#selectElement',
-    settings: {
+fetchBreeds()
+  .then(response => {
+    elements.selectCat.hidden = false;
+    elements.selectCat.innerHTML = response
+      .map(({ id, name }) => {
+        return `<option class = "option" value="${id}">${name}</option>`;
+      })
+      .join('');
+
+    new SlimSelect({
+      select: '.breed-select',
+      settings: {
         showSearch: false,
         searchText: 'Ментор, вибачай, але нічого ти тут не побачиш!!!',
         searchPlaceholder: 'Знайдіть щось хороше!!',
-        searchHighlight: true
-      }, 
-      events:{
-        afterChange: newVal => {
-        elements.loaderText.hidden = false;
-    elements.infoCat.innerHTML = "";
-fetchCatByBreed(newVal[0].value).then(response => {
-elements.infoCat.innerHTML = response.map(({ url, beeds: [{ name, description, temperament}] }) => {
-    return `
+        searchHighlight: true,
+      },
+      
+      onChange: (newVal) => {
+          elements.loaderText.hidden = false;
+          elements.infoCat.innerHTML = '';
+
+          fetchCatByBreed(newVal.value)
+            .then((response) => {
+              elements.infoCat.innerHTML = response
+                .map(({ url, breeds: [{ name, description, temperament }] }) => {
+                  return `
     <div class = "cat_photo">
     <img src="${url}" alt="${name}" ">
     </div>
@@ -36,14 +43,18 @@ elements.infoCat.innerHTML = response.map(({ url, beeds: [{ name, description, t
 <h3>${name}</h3>
 <p>${description}</p>
 <p><b>Temperament:&nbsp</b>${temperament}</p></div>`;
-}
-).join("");
-}).catch(() =>{
-    Notiflix.Notify.failure(elements.errorText.textContent);
-}).finally(() => (elements.loaderText.hidden = true))
-}   
-    }
+                })
+                .join('');
+            })
+            .catch(() => {
+              Notiflix.Notify.failure(elements.errorText.textContent);
+            })
+            .finally(() => {elements.loaderText.hidden = true;
+        }),
+    
+    );
   })
-}).catch(() => {
+  .catch(() => {
     Notiflix.Notify.failure(elements.errorText.textContent);
-}).finally(() => (elements.loaderText.hidden = true))
+  })
+  .finally(() => {elements.loaderText.hidden = true});
