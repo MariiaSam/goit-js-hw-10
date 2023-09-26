196671e918f184e3d096b5d762e92f0455821fbf
MariiaSam <kontragent@ukr.net>
2023-09-26 13:37:54 +0300

    corr

--- src/cat-api.js
+++ src/cat-api.js
@@ -4,12 +4,14 @@ import axios from 'axios';
 //   'live_msdyLXd6fQ4XqkQOafThG3QlqI9pVAyJsQYWr21gOKoQrJvn3mlevFEdkllUWaMq';
 // // axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
 
-export async function fetchBreeds() {
-  const { data } = await axios.get('breeds');
+export function fetchBreeds() {
+  return axios.get('breeds').then(({ data }) => {
     return data;
+  });
 }
 
-export async function fetchCatByBreed(breedId) {
-    const { data } = await axios.get(`images/search?breed_ids=${breedId}`);
-    return data;
+export function fetchCatByBreed(breedId) {
+    return axios.get(`images/search?breed_ids=${breedId}`).then(({ data }) => {
+      return data;
+    });
   }
--- src/index.js
+++ src/index.js
@@ -10,28 +10,28 @@ axios.defaults.headers.common['x-api-key'] =
 axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
 
 fetchBreeds()
-  .then((response) => {
+  .then(response => {
     elements.selectCat.hidden = false;
-    elements.selectCat.innerHTML = response
-      .map(({ id, name }) => {
+    elements.selectCat.innerHTML = response.map(({ id, name }) => {
         return `<option class = "option" value="${id}">${name}</option>`;
       })
       .join('');
 
     new SlimSelect({
-      select: '.breed-select',
+      select: '#single',
       settings: {
         showSearch: false,
         searchText: 'Ментор, вибачай, але нічого ти тут не побачиш!!!',
         searchPlaceholder: 'Знайдіть щось хороше!!',
         searchHighlight: true,
       },
-      onChange: (newVal) => {
+      events: {
+        afterChange: newVal => {
           elements.loaderText.hidden = false;
           elements.infoCat.innerHTML = '';
 
           fetchCatByBreed(newVal[0].value)
-            .then((response) => {
+            .then(response => {
               elements.infoCat.innerHTML = response
                 .map(({ url, breeds: [{ name, description, temperament }] }) => {
                   return `
@@ -48,9 +48,11 @@ fetchBreeds()
             .catch(() => {
               Notiflix.Notify.failure(elements.errorText.textContent);
             })
-            .finally(() => {elements.loaderText.hidden = true});
+            .finally(() => (elements.loaderText.hidden = true));
         },
- })
+ }
+}
+)
   })
   .catch(() => {
     Notiflix.Notify.failure(elements.errorText.textContent);
