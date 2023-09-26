f3f9b4863217a444802ad69cd835817e13fcdf7c
MariiaSam <kontragent@ukr.net>
2023-09-26 14:00:07 +0300

    corr

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,17 +1,27 @@
 import axios from 'axios';
 
-// axios.defaults.headers.common['x-api-key'] =
-//   'live_msdyLXd6fQ4XqkQOafThG3QlqI9pVAyJsQYWr21gOKoQrJvn3mlevFEdkllUWaMq';
-// // axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
+axios.defaults.headers.common['x-api-key'] =
+  'live_msdyLXd6fQ4XqkQOafThG3QlqI9pVAyJsQYWr21gOKoQrJvn3mlevFEdkllUWaMq';
+
+  const  API_KEY = 'live_msdyLXd6fQ4XqkQOafThG3QlqI9pVAyJsQYWr21gOKoQrJvn3mlevFEdkllUWaMq';
+
+const urlBreeds = 'https://api.thecatapi.com/v1/breeds';
+const urlCat = 'https://api.thecatapi.com/v1/images';
 
 export function fetchBreeds() {
-  return axios.get('breeds').then(({ data }) => {
-    return data;
-  });
+  return axios.get(`${urlBreeds}`).then(response => {
+  if(!response.status === 200) {
+    throw new Error (response.statusText);
+  } 
+return response.data;
+})
 }
 
 export function fetchCatByBreed(breedId) {
-    return axios.get(`images/search?breed_ids=${breedId}`).then(({ data }) => {
-      return data;
-    });
-  }
+  return axios.get(`${urlCat}?breed_ids=${breedId}}`).then(response => {
+    if(!response.status === 200) {
+      throw new Error (response.statusText);
+    } 
+  return response.data;
+  })
+}
