13a6f69628ffb183b8d00a48308438c043c56dd2
MariiaSam <kontragent@ukr.net>
2023-09-27 11:50:50 +0300

    css

--- src/cat-api.js
+++ src/cat-api.js
@@ -4,14 +4,16 @@ axios.defaults.headers.common['x-api-key'] =
   'live_msdyLXd6fQ4XqkQOafThG3QlqI9pVAyJsQYWr21gOKoQrJvn3mlevFEdkllUWaMq';
 axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
 
+// ф-ція для отримання спискуи порід
 export function fetchBreeds() {
   return axios.get('breeds').then(({ data }) => {
     return data;
   });
 }
 
+// ф-ція ідентифікатор породиь  => СПИСОК об, які предст котів певної породи
 export function fetchCatByBreed(breedId) {
-    return axios.get(`images/search?breed_ids=${breedId}`).then(({ data }) => {
-      return data;
-    });
-  }
+  return axios.get(`images/search?breed_ids=${breedId}`).then(({ data }) => {
+    return data;
+  });
+}
